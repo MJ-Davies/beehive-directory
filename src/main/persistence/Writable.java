307cdedd87429b4f84ea895d310e744cd122d0b4
Mary-Jane Davies <160426017+MJ-Davies@users.noreply.github.com>
2024-04-15 11:07:14 -0700

    Update Writable.java
    Removed citation to a UBC project provided by UBC's computer science course.

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -3,7 +3,6 @@ package persistence;
 import org.json.JSONObject;
 
 // Represents classes which can be written with Json
-// Modelled from github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo/blob/master/src/main/persistence/Writable.java
 public interface Writable {
     // EFFECTS: Converts this object into a readable Json Object
     JSONObject toJson();
